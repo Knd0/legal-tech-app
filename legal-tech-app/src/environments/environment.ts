84573934d6632974d40364e4166f6407bc55968f
Franco de Iriondo <luafranco09@hotmail.com>
2026-02-17 20:13:00 -0300

    fix(frontend): force production API URL in default environment file to resolve Vercel connection issue

--- legal-tech-app/src/environments/environment.ts
+++ legal-tech-app/src/environments/environment.ts
@@ -1,6 +1,5 @@
-export const environment = {
   production: false,
   // REEMPLAZAR CON TU CLAVE PÚBLICA GENERADA (npx web-push generate-vapid-keys)
   vapidPublicKey: 'BGPqWkOJnTCyfEjBbLM3ZzQ2M0lOy1JWxRwCU1DzONfffH3JnfQM9x2FurRkU3DxYkpN4zBbfNqyov31Yqpha5Ck',
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://legal-tech-app-gdme.onrender.com'
 };

ebbfbc849b1c721b20b5a4ad892269cd2f6731bd
Franco de Iriondo <luafranco09@hotmail.com>
2026-02-17 20:18:19 -0300

    fix(frontend): restore malformed environment.ts file

--- legal-tech-app/src/environments/environment.ts
+++ legal-tech-app/src/environments/environment.ts
@@ -1,3 +1,4 @@
+export const environment = {
   production: false,
   // REEMPLAZAR CON TU CLAVE PÚBLICA GENERADA (npx web-push generate-vapid-keys)
   vapidPublicKey: 'BGPqWkOJnTCyfEjBbLM3ZzQ2M0lOy1JWxRwCU1DzONfffH3JnfQM9x2FurRkU3DxYkpN4zBbfNqyov31Yqpha5Ck',
